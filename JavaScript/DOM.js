449d85c85bd20522ca210a083e60dd99208ab30d
maphuti2 <maphuti22r@gmail.com>
2024-09-12 09:26:00 +0200

    Practicing DOM manipulation

---
+++ JavaScript/DOM.js
@@ -0,0 +1,5 @@
+const container = document.querySelector("#container");
+console.dir(container.firstElementChild);
+
+const control = document.querySelector(".control");
+console.dir(control.previousElementSibling);
